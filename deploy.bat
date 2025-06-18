37ae61b7304910c9aa29760d75b3ebe808d4a6fc
chengronggang <chengronggang@xylink.com>
2025-06-18 16:45:21 +0800

    update deploy config

---
+++ deploy.bat
@@ -0,0 +1,18 @@
+@echo off
+
+REM 构建
+call npm run build
+
+REM 进入生成的构建文件夹
+cd dist
+
+REM 初始化 git
+git init
+git add -A
+git commit -m "deploy"
+
+REM 推送到 gh-pages 分支
+git push -f git@github.com:dander521/official-website.git master:gh-pages
+
+REM 返回上级目录
+cd .. 
--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/official-website.git master:gh-pages
+git push -f git@github.com:dander521/official-website.git master:gh-pages
 
 cd - 

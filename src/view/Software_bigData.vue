eb6f2e58874a94902ff1d4968c214f05115a25c2
RogerChen <123020990@qq.com>
2025-07-01 17:57:44 +0800

    bigdata

--- src/view/Software_bigData.vue
+++ src/view/Software_bigData.vue
@@ -3,8 +3,14 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h1>Chemicals</h1>
+                    <h1>Chemicals for propellant</h1>
                 </div>
+
+                <!-- 显示两段文字 -->
+                <p>We supply a variety of products for use in defense and aerospace industries. especially chemical raw materials to be used in NC-based propellant and in solid composite propellant.</p>
+                <p>Authorized by major Chinese producers as their Exclusive Sales Agent for Overseas market, we promise to our customers: Transparent business background, Guaranteed quality, and Steady supply.</p>
+            
+                
             </div>
         </div>
     </div>
